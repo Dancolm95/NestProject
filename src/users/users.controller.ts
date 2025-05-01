9577c0bc205fb89e61b65ec606d9c5241f6bcc88
Daniel Colona <dancolm95@gmail.com>
2025-05-01 17:32:30 -0500

    add crud users with validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,48 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Patch,
+  Param,
+  ParseIntPipe,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { User } from './Entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly userService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
 
   @Get()
-  getHello(): string {
-    return this.usersService.getMessage();
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number){
+    return this.userService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.userService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    this.userService.remove(id);
+    return { message: `Usuario con ID ${id} eliminado` };
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,42 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { User } from './Entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 @Injectable()
 export class UsersService {
-  getMessage(): string {
-    return 'Hola desde el user service';
+  private users: User[] = [];
+  private idCounter = 1;
+
+  create(user: CreateUserDto): User {
+    const newUser = { id: this.idCounter++, ...user };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  findAll(): User[] {
+    return this.users;
+  }
+
+  findOne(id: number): User {
+    const user = this.users.find((u) => u.id === id);
+    if (!user) {
+      throw new NotFoundException(`Usuario con ID ${id} no encontrado`);
+    }
+    return user;
+  }
+
+  update(id: number, updatedData: Partial<User>): User {
+    const user = this.findOne(id);
+    const updatedUser = { ...user, ...updatedData };
+    const index = this.users.findIndex((u) => u.id === id);
+    this.users[index] = updatedUser;
+    return updatedUser;
+  }
+
+  remove(id: number): void {
+    const index = this.users.findIndex((u) => u.id === id);
+    if (index === -1) {
+      throw new NotFoundException(`Usuario con ID ${id} no encontrado`);
+    }
+    this.users.splice(index, 1);
   }
 }
