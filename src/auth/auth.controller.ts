0f7fb1cadb9e8d531e6001d44696bc9736fa728a
Daniel Colona <dancolm95@gmail.com>
2025-05-07 21:29:18 -0500

    fix JWT authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,10 +7,6 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @Post('login')
   async login(@Body() body: { email: string; password: string }) {
-    const user: User = await this.authService.validateUser(
-      body.email,
-      body.password
-    );
-    return this.authService.login(user);
+    return this.authService.login(body.email, body.password);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,8 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
-import { User } from 'src/users/Entities/user.entity';
-import { AuthenticatedUser } from './auth.types';
+import { UserResponseDto } from 'src/users/dto/userResponse';
 
 @Injectable()
 export class AuthService {
@@ -11,18 +10,19 @@ export class AuthService {
     private userService: UsersService,
     private jwtService: JwtService
   ) {}
-  async validateUser(email: string, password: string): Promise<User> {
+  async login(
+    email: string,
+    password: string
+  ): Promise<{ access_token: string; user: UserResponseDto }> {
     const user = await this.userService.findByEmail(email);
     if (user && (await bcrypt.compare(password, user.password))) {
-      const { password, ...rest } = user;
-      return rest;
+      const { password: _, ...rest } = user;
+      const payload = { sub: user.id, email: user.email };
+      return {
+        access_token: this.jwtService.sign(payload),
+        user: rest,
+      };
     }
     throw new UnauthorizedException('Credenciales inválidas');
-  }
-  login(user: AuthenticatedUser) {
-    const payload = { sub: user.id, email: user.email };
-    return {
-      access_token: this.jwtService.sign(payload),
-    };
   }
 }
