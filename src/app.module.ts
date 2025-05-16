81be9cf4365d4fd0542f50036068aecc4bd074a9
Daniel Colona <dancolm95@gmail.com>
2025-05-16 16:21:29 -0500

    use AuthGuard('jwt') how global guard using APP_GUARD

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,30 +6,44 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { config } from 'process';
+import { APP_GUARD } from '@nestjs/core';
+
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    UsersModule,
+
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (config: ConfigService): TypeOrmModuleOptions => ({
-        host: config.get('DB_HOST'),
-        port: parseInt(config.get<string>('DB_PORT', '5432')),
-        username: config.get('DB_USERNAME'),
-        password: config.get<string>('DB_PASSWORD'),
-        database: config.get<string>('DB_NAME'),
-        entities: [User],
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
+      useFactory: (config: ConfigService): TypeOrmModuleOptions => {
+        const options: TypeOrmModuleOptions = {
+          type: 'postgres',
+          host: config.get<string>('DB_HOST'),
+          port: parseInt(config.get<string>('DB_PORT', '5432')),
+          username: config.get<string>('DB_USERNAME'),
+          password: config.get<string>('DB_PASSWORD'),
+          database: config.get<string>('DB_NAME'),
+          entities: [User], // o autoLoadEntities: true si prefieres automático
+          synchronize: true,
+        };
+
+        console.log('🔍 Conectando con:', options);
+        return options;
+      },
     }),
+
+    UsersModule,
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+  }],
 })
 export class AppModule {}
